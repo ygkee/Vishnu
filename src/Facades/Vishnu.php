c32645a01d0486ade49c9a950475217b8484d5dd
kee <kee.yooog@gmail.com>
2016-02-15 17:45:46 +0800

    add facade

---
+++ src/Facades/Vishnu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace YK\Vishnu\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Vishnu extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'Facade';
+    }
+}
